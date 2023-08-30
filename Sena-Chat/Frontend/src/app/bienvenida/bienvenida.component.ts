b3e41d71ce7ad2536a65bc9a353b5026252853ab
JhoanDucu <114232996+JhoanDucu@users.noreply.github.com>
2023-08-30 16:49:49 -0500

    Update bienvenida.component.ts
    Se agrega el Routing de la bienvenida hacia el chat

--- Sena-Chat/Frontend/src/app/bienvenida/bienvenida.component.ts
+++ Sena-Chat/Frontend/src/app/bienvenida/bienvenida.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { LogearService } from '../Servicios/logear.service';
 import { ActivatedRoute,Params } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bienvenida',
@@ -11,11 +12,12 @@ import { ActivatedRoute,Params } from '@angular/router';
 export class BienvenidaComponent {
   constructor( 
     private loginServicio: LogearService,
-    private rutaActiva: ActivatedRoute 
+    private rutaActiva: ActivatedRoute,
+    private router: Router
     ){}
   setFicha( ficha: ''){
     this.loginServicio.seleccionarFicha({buscar: ficha}, this.rutaActiva.snapshot.params['usuario']).subscribe((respuesta: any) => {
-      alert(respuesta);
+      if(respuesta == 'Actualizado') this.router.navigate(['chat']);
     });
   }
 }
