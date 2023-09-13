74d01529097147b1adc0b6791c25dc14901e7faf
JUAN CARDENAS PEREZ <114235553+Juanperez23215698@users.noreply.github.com>
2023-09-13 13:13:22 -0500

    Add files via upload
    actualización de estilos

--- Sena-Chat/Frontend/src/app/confi/confi.component.ts
+++ Sena-Chat/Frontend/src/app/confi/confi.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-confi',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./confi.component.css']
 })
 export class ConfiComponent {
+  password: string = '';
+  showPassword: boolean = false;
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  password2: string = '';
+  showPassword2: boolean = false;
+
+  togglePasswordVisibility2(): void {
+    this.showPassword2 = !this.showPassword2;
+  }
+  
 
 }
