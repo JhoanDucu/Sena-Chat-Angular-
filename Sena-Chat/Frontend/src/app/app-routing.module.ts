cee668021f6a41c727cc4824cd50d8d2d663d26d
JhoanDucu <114232996+JhoanDucu@users.noreply.github.com>
2023-09-18 17:39:36 -0500

    Update app-routing.module.ts

--- Sena-Chat/Frontend/src/app/app-routing.module.ts
+++ Sena-Chat/Frontend/src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { ConfiComponent } from './confi/confi.component'; 
+import { ContrasenaComponent } from './contrasena/contrasena.component';
+import { VisPerfilComponent } from './vis-perfil/vis-perfil.component';
 
 const routes: Routes = [
   { path: 'home', component: AppComponent},
   { path: "registro", component: RegistroComponent},
   { path: "login", component: LoginComponent},
-  { path: 'chat', component: ChatComponent},
+  { path: 'chat/:documento/:ficha/:grupo', component: ChatComponent},
   { path: 'bienvenida/:usuario', component: BienvenidaComponent},
+  { path: 'confi', component: ConfiComponent },
+  { path: 'contrasena', component: ContrasenaComponent},
+  { path: 'vis-perfil', component: VisPerfilComponent},
 ];
 
 @NgModule({
