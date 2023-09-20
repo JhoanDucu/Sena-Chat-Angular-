2c1e78f5f60da2fb803b80798b5fbc1b5c0e0715
JUAN CARDENAS PEREZ <114235553+Juanperez23215698@users.noreply.github.com>
2023-09-19 23:41:54 -0500

    Update vis-perfil.component.ts

--- Sena-Chat/Frontend/src/app/vis-perfil/vis-perfil.component.ts
+++ Sena-Chat/Frontend/src/app/vis-perfil/vis-perfil.component.ts
@@ -1,16 +1,37 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { VisPerfilService } from '../Servicios/vis-perfil.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute,Params } from '@angular/router';
+import { Usuario } from '../modelos/usuarios';
 
-
-@Injectable({
-  providedIn: 'root'
+@Component({
+  selector: 'app-vis-perfil',
+  templateUrl: './vis-perfil.component.html',
+  styleUrls: ['./vis-perfil.component.css']
 })
-export class VisPerfilService {
+export class VisPerfilComponent {
+  constructor(
+    private router: Router,
+    private VisPerfilService: VisPerfilService,
+    private rutaActiva: ActivatedRoute
+    ){}
 
-  url = "http://localhost:3000";
-  constructor(private http: HttpClient) {}
+  public item: Usuario[] = [];
+  numerodoc = '';
 
-  buscarDatos(datos: any){
-    return this.http.get(`${this.url}/usuario/${datos}`);
+  // usuario = {
+  //   primerNombre: 'Juan',
+  //   segundoNombre: 'David',
+  //   primerApellido: 'Cardenas',
+  //   segundoApellido: 'Perez',
+  //   nombreUsuario: 'juan_cardenas',
+  //   numeroDocumento: '1131104356',
+  //   correoElectronico: 'juan.cardenas34@misena.edu.co',
+  // };
+
+  ngOnInit(): void { 
+    this.VisPerfilService.buscarDatos(this.rutaActiva.snapshot.params['documento']).subscribe((data:any)=> data.forEach((value: any)=> this.item.push(value)));
   }
+
+  
 }
