814be57c138693f137905ddbb3a3cc4561f43c47
JUAN CARDENAS PEREZ <114235553+Juanperez23215698@users.noreply.github.com>
2023-09-19 23:16:39 -0500

    Update vis-perfil.service.ts

--- Sena-Chat/Frontend/src/app/Servicios/vis-perfil.service.ts
+++ Sena-Chat/Frontend/src/app/Servicios/vis-perfil.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class VisPerfilService {
 
-  constructor() { }
+  url = "http://localhost:3000";
+  constructor(private http: HttpClient) {}
+
+  buscarDatos(datos: any){
+    return this.http.get(`${this.url}/usuario/${datos}`);
+  }
 }
