a025ae3453b0bac58692f1b79b36687ba49d9af2
JhoanDucu <114232996+JhoanDucu@users.noreply.github.com>
2023-10-05 12:05:09 -0500

    Update app.component.ts

--- Sena-Chat/Frontend/src/app/app.component.ts
+++ Sena-Chat/Frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'Sena-Chat';
+  constructor(private elementRef: ElementRef) {}
+  ngAfterViewInit() {
+      this.elementRef.nativeElement.ownerDocument.body.style.backgroundColor = ' rgb(165, 163, 163)';
+  }
 }
