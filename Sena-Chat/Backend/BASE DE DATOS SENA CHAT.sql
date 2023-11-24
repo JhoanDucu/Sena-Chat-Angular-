0827111103c7a570c684888a47c8b2e876d910f1
JhoanDucu <114232996+JhoanDucu@users.noreply.github.com>
2023-11-23 20:09:14 -0500

    Update BASE DE DATOS SENA CHAT.sql
    Cambios para mensajes privados

--- Sena-Chat/Backend/BASE DE DATOS SENA CHAT.sql
+++ Sena-Chat/Backend/BASE DE DATOS SENA CHAT.sql
@@ -28,9 +28,10 @@ create table mensaje
 	fecha DATE NOT NULL,
 	hora TIME NOT NULL,
 	contenido_mensaje VARCHAR(10000) NOT NULL,
-	numerodoc VARCHAR(20) NOT NULL,
+	fk_destino INT NOT NULL,
+   # numerodoc VARCHAR(20) NOT NULL,  		CAMBIAR RELACIONES
+   # fk_id_grupos INT NOT NULL,
 	id_tipo INT NOT NULL,
-	fk_id_grupos INT NOT NULL,
 	PRIMARY KEY (id_mensaje)
 );
 
@@ -44,12 +45,13 @@ create table tipo_mensaje
 
 create table grupos
 (
-	id_grupos INT NOT NULL,
+	id_grupos INT NOT NULL AUTO_INCREMENT,
 	nom_grupos VARCHAR(20) NOT NULL,
 	descripcion_grupos VARCHAR(50) NOT NULL,
 	id_ficha INT NOT NULL,
+	fk_tipo_grupo INT NOT NULL,
 	PRIMARY KEY (id_grupos)
-); 
+);
 
 create table usuarios
 (
@@ -74,12 +76,24 @@ create table usuarios_grupos
 	id_grupos INT NOT NULL,
 	numerodoc VARCHAR(20) NOT NULL,
 	PRIMARY KEY (id_usuarios_grupos, id_grupos, numerodoc)
-); 
+);
+
+create table tipo_grupo
+(
+	id_tipo_grupo INT NOT NULL,
+	descripcion_tipo_grupos VARCHAR(20) NOT NULL,
+	PRIMARY KEY(id_tipo_grupo)
+);
 
 ALTER TABLE grupos 
 ADD CONSTRAINT PK_FK_id_ficha 
 FOREIGN KEY (id_ficha) 
 REFERENCES ficha (id_ficha);
+
+ALTER TABLE grupos 
+ADD CONSTRAINT tipoGrupo 
+FOREIGN KEY (fk_tipo_grupo) 
+REFERENCES tipo_grupo(id_tipo_grupo);
 
 ALTER TABLE usuarios_grupos
 ADD CONSTRAINT FK_PK_id_grupos
@@ -107,54 +121,53 @@ FOREIGN KEY (fk_id_ficha)
 REFERENCES ficha (id_ficha);
 
 ALTER TABLE mensaje
-ADD CONSTRAINT FK_PK_id_usuario
-FOREIGN KEY (numerodoc)
-REFERENCES usuarios (numerodoc);
+ADD CONSTRAINT FK_Destino
+FOREIGN KEY (fk_destino)
+REFERENCES usuarios_grupos(id_usuarios_grupos);
 
 ALTER TABLE mensaje
 ADD CONSTRAINT FK_PK_id_tipo
 FOREIGN KEY (id_tipo)
 REFERENCES tipo_mensaje (id_tipo);
 
-ALTER TABLE mensaje
-ADD CONSTRAINT FK_PK_id_grupo
-FOREIGN KEY (fk_id_grupos)
-REFERENCES grupos (id_grupos);
-
-INSERT INTO ficha 
-VALUES ('2558101','Analisis Y Desarrollo De Software'),
-	   ('2558102','Arte, cultura, esparcimiento y deportes'),
-	   ('2558103','Tecnologías de la información'),
-	   ('2558104','Finanzas'),
-       ('2558105','administración'),
-	   ('2558106','Salud'),
-	   ('2558107','Operación de equipo industrial'),
-	   ('2558108','Ciencias sociales'),
-	   ('2558109','servicios gubernamentales'),
-	   ('25581010', 'Desarrollo de Software'),
+INSERT INTO ficha VALUES 
+('2558101','Analisis Y Desarrollo De Software'),
+('2558102','Arte, cultura, esparcimiento y deportes'),
+('2558103','Tecnologías de la información'),
+('2558104','Finanzas'),
+('2558105','administración'),
+('2558106','Salud'),
+('2558107','Operación de equipo industrial'),
+('2558108','Ciencias sociales'),
+('2558109','servicios gubernamentales'),
+('25581010', 'Desarrollo de Software'),
 ('2558111', 'Gestión de Recursos Humanos'),
 ('2558112', 'Mecatrónica Industrial'),
 ('2558113', 'Gastronomía'),
 ('2558114', 'Tecnología en Redes de Computadores');
 
-INSERT INTO roles 
-VALUES ('1','INSTRUCTOR'),
+INSERT INTO roles VALUES 
+('1','INSTRUCTOR'),
 ('2','APRENDIZ');
 
-INSERT INTO tipo_documento 
-VALUES ('1','Cédula de Ciudadanía'),
-	   ('2','Tarjeta de Identidad'),
-	   ('3','Cédula de Extranjería'),
-	   ('4','PEP'),
-	   ('5','Permiso por Protección Temporal');
+INSERT INTO tipo_documento VALUES 
+('1','Cédula de Ciudadanía'),
+('2','Tarjeta de Identidad'),
+('3','Cédula de Extranjería'),
+('4','PEP'),
+('5','Permiso por Protección Temporal');
 
-INSERT INTO tipo_mensaje 
-VALUES ('1','Mensaje de Texto','text'),
-	   ('2','Imagen', 'img'),
-	   ('3','Documento', 'docx');
+INSERT INTO tipo_mensaje VALUES 
+('1','Mensaje de Texto','text'),
+('2','Imagen', 'img'),
+('3','Documento', 'docx');
 
-INSERT INTO usuarios 
-VALUES ('juan.cardenas34@misena.edu.co','juan','david','cardenas','perez',MD5('123'),'juan_cardenas',NULL,'2',1131104356,1, '2558104'),
+INSERT INTO tipo_grupo VALUES
+(1, 'Privado'),
+(2, 'Grupal');
+
+INSERT INTO usuarios VALUES 
+('juan.cardenas34@misena.edu.co','juan','david','cardenas','perez',MD5('123'),'juan_cardenas',NULL,'2',1131104356,1, '2558104'),
 ('camilo@gmail.com','camilo',NULL,'perez',NULL,MD5('123'),'carlosperez',NULL,'2','1234567911',1,'2558104'),
 ('sebastian@gmail.com','sebastian',NULL,'carrillo',NULL,MD5('123'),'sebastian_123',NULL,'2','12345678912',1,'2558105'),
 ('isabella.mitchell@example.com','isabella',NULL,'mitchell',NULL,MD5('123'),'isabella',NULL,'2','12345678913',1,'2558102'),
@@ -185,84 +198,85 @@ VALUES ('juan.cardenas34@misena.edu.co','juan','david','cardenas','perez',MD5('1
 ('miaparker@example.com', 'Mia', NULL, 'Parker', NULL, MD5('passwordabc'), 'miap', 'mia.jpg', '2', '12345678019', 1, '2558103'),
 ('lucasbrown@example.com', 'Lucas', NULL, 'Brown', NULL, MD5('passwordxyz'), 'lucasb', 'lucas.jpg', '2', '12345678020', 2, '2558103');
 
-INSERT INTO grupos 
-VALUES ('1','grupo 1','grupo de Heiver, ficha 2558104','2558104'),
-	   ('2','grupo 2','grupo de Leonardo, ficha 2558105','2558105'),
-	   ('3','grupo 3','grupo de Javier, ficha 2558104','2558104'),
-	   ('4','grupo 4','grupo de Isaura, ficha 2558104','2558104'),
-	   ('5','grupo 5','grupo de Wendy, ficha 2558105','2558105'),
-	   ('6','grupo 6','grupo de Alejandra, ficha 2558105','2558105'),
-	   ('7','grupo 7','grupo de Manolo, ficha de 2558102','2558102'),
-	   ('8', 'Grupo 8', 'Grupo de sofia', '2558111'),
-('9', 'Grupo 9', 'Grupo de sebas', '2558112'),
-('10', 'Grupo 10', 'Grupo de lorenso', '2558112'),
-('11', 'Grupo 11', 'Grupo de camilo', '2558103'),
-('12', 'Grupo 12', 'Grupo de tulia', '2558105'),
-('13', 'Grupo 13', 'Grupo de nicolas', '2558106'),
-('14', 'Grupo 14', 'Grupo de esteban', '2558107'),
-('15', 'Grupo 15', 'Grupo de maria', '2558105'),
-('16', 'Grupo 16', 'Grupo de camila', '2558102'),
-('17', 'Grupo 17', 'Grupo de lorena', '2558112'),
-('18', 'Grupo 18', 'Grupo de laura', '2558113'),
-('19', 'Grupo 19', 'Grupo de jhoan', '2558112'),
-('20', 'Grupo 20', 'Grupo de carlos', '2558113');
+INSERT INTO grupos VALUES
+('1','grupo 1','grupo de Heiver, ficha 2558104','2558104',2),
+('2','grupo 2','grupo de Leonardo, ficha 2558105','2558105',2),
+('3','grupo 3','grupo de Javier, ficha 2558104','2558104',2),
+('4','grupo 4','grupo de Isaura, ficha 2558104','2558104',2),
+('5','grupo 5','grupo de Wendy, ficha 2558105','2558105',2),
+('6','grupo 6','grupo de Alejandra, ficha 2558105','2558105',2),
+('7','grupo 7','grupo de Manolo, ficha de 2558102','2558102',2),
+('8', 'Grupo 8', 'Sofia', '2558104',1),
+('9', 'Grupo 9', 'Sebas', '2558112',1),
+('10', 'Grupo 10', 'Lorenso', '2558112',1),
+('11', 'Grupo 11', 'Camilo', '2558103',1),
+('12', 'Grupo 12', 'Tulia', '2558105',1),
+('13', 'Grupo 13', 'Nicolas', '2558106',1),
+('14', 'Grupo 14', 'Esteban', '2558107',1),
+('15', 'Grupo 15', 'Maria', '2558105',1),
+('16', 'Grupo 16', 'Camila', '2558102',1),
+('17', 'Grupo 17', 'Lorena', '2558112',1),
+('18', 'Grupo 18', 'Laura', '2558113',1),
+('19', 'Grupo 19', 'Jhoan', '2558112',1),
+('20', 'Grupo 20', 'Carlos', '2558113',1);
 
-INSERT INTO mensaje 
-VALUES ('1', '2020-03-20','11:30:00','HOLA','1131104356', '1','1'),
-	('2', '2020-03-20','12:30:00','BUEN DIA, INSTRUCTOR','1021392807', '1','1'),
-	('3', '2020-03-20','13:30:00','QUE TRANZA','1131104356','1','2'),
-	('4', '2020-03-20','11:30:00','PERROS WOOF WOOF','12345678012', '1','1'),
-	('5', '2020-03-20','12:30:00','ALAMBRE DE QUESO','1021392807', '1','2'),
-	('6', '2020-03-20','13:30:00','SI :D','1021392807','1','2'),
-	('7', '2023-09-19', '14:45:00', 'ya', '1131104356', '1', '1'),
-('8', '2023-09-19', '15:00:00', 'no se', '12345678912', '1', '1'),
-('9', '2023-09-19', '15:15:00', 'jijija', '1131104356', '1', '3'),
-   ('10', '2023-09-19', '15:30:00', 'buenos dias profe', '12345678916', '1', '3'),
-   ('11', '2023-09-19', '15:45:00', 'ta to bien', '12345678917', '1', '2'),
-   ('12', '2023-09-19', '16:00:00', 'ola', '12345678918', '1', '1'),
-   ('13', '2023-09-19', '16:15:00', 'ya yo ya', '1131104356', '1', '3'),
-   ('14', '2023-09-19', '16:30:00', 'jaijsiajsijijsi', '12345678912', '1', '2'),
-   ('15', '2023-09-19', '16:45:00', 'no', '1021392807', '1', '2'),
-   ('16', '2023-09-19', '17:00:00', 'ayer me tome clorox', '1131104356', '1', '4'),
-   ('17', '2023-09-19', '17:15:00', 'no es excusas ', '12345678912', '1', '1'),
-   ('18', '2023-09-19', '17:30:00', 'es problema del indio', '12345678914', '1', '5'),
-   ('19', '2023-09-19', '17:45:00', 'la defeco', '12345678916', '1', '4'),
-   ('20', '2023-09-19', '18:00:00', 'no se chino', '12345678917', '1', '1'),
-   ('21', '2023-09-19', '18:15:00', 'colombia es gooood', '1131104356', '1', '4'),
-   ('22', '2023-09-19', '18:30:00', 'viva cristo rey', '12345678919', '1', '5'),
-   ('23', '2023-09-19', '18:45:00', 'nico lindo', '1131104356', '1', '2'),
-   ('24', '2023-09-19', '19:00:00', 'jhoan mi rey', '1021392807', '1', '3'),
-   ('25', '2023-09-19', '19:15:00', 'jhuan mi x100pre', '1131104356', '1', '3'),
-   ('26', '2023-09-19', '19:30:00', 'ilovemi', '12345678912', '1', '2');
+INSERT INTO usuarios_grupos VALUES 
+(NULL,'1','1131104356'),   #1
+(NULL,'8','1131104356'),   #2
+(NULL,'4','1131104356'),   #3
+(NULL,'1','1234567911'),   #4
+(NULL,'3','1234567911'),    #5
+(NULL,'4','1234567911'),    #6
+(NULL,'1','12345678914'),    #7
+(NULL,'3','12345678914'),    #8
+(NULL,'4','12345678914'),    #9
+(NULL,'1','12345678916'),    #10
+(NULL,'3','12345678916'),    #11
+(NULL,'4','12345678916'),    #12
+(NULL,'1','12345678917'),    #13
+(NULL,'3','12345678917'),    #14
+(NULL,'4','12345678917'),    #15
+(NULL,'1','12345678918'),    #16
+(NULL,'3','12345678918'),    #17
+(NULL,'4','12345678918'),    #18
+(NULL,'1','12345678919'),    #19
+(NULL,'3','12345678919'),    #20
+(NULL,'4','12345678919'),    #21
+(NULL,'1','12345678912'),    #22
+(NULL,'5','12345678912'),    #23
+(NULL,'6','12345678912'),    #24
+(NULL,'1','1021392807'),    #25
+(NULL,'5','1021392807'),    #26
+(NULL,'6','1021392807'),    #27
+(NULL,'1','12345678012');    #28
 
-INSERT INTO usuarios_grupos 
-VALUES (NULL,'1','1131104356'),
-		(NULL,'3','1131104356'),
-	   	(NULL,'4','1131104356'),
-		(NULL,'1','1234567911'),
-		(NULL,'3','1234567911'),
-	   	(NULL,'4','1234567911'),
-		(NULL,'1','12345678914'),
-		(NULL,'3','12345678914'),
-	   	(NULL,'4','12345678914'),
-		(NULL,'1','12345678916'),
-		(NULL,'3','12345678916'),
-	   	(NULL,'4','12345678916'),
-		(NULL,'1','12345678917'),
-		(NULL,'3','12345678917'),
-	   	(NULL,'4','12345678917'),
-		(NULL,'1','12345678918'),
-		(NULL,'3','12345678918'),
-	   	(NULL,'4','12345678918'),
-		(NULL,'1','12345678919'),
-		(NULL,'3','12345678919'),
-	   	(NULL,'4','12345678919'),
-		(NULL,'2','12345678912'),
-		(NULL,'5','12345678912'),
-		(NULL,'6','12345678912'),
-		(NULL,'2','1021392807'),
-		(NULL,'5','1021392807'),
-		(NULL,'6','1021392807');
+INSERT INTO mensaje VALUES
+('1', '2020-03-20','11:30:00','HOLA','1','1'),
+('2', '2020-03-20','12:30:00','BUEN DIA, INSTRUCTOR','26','1'),
+('3', '2020-03-20','13:30:00','QUE TRANZA','1','2'),
+('4', '2020-03-20','11:30:00','PERROS WOOF WOOF','28','1'),
+('5', '2020-03-20','12:30:00','ALAMBRE DE QUESO','25','2'),
+('6', '2020-03-20','13:30:00','SI :D','25','2'),
+('7', '2023-09-19', '14:45:00', 'ya', '1', '1'),
+('8', '2023-09-19', '15:00:00', 'no se', '22', '1'),
+('9', '2023-09-19', '15:15:00', 'jijija', '1', '3'),
+('10', '2023-09-19', '15:30:00', 'buenos dias profe', '10', '3'),
+('11', '2023-09-19', '15:45:00', 'ta to bien', '13', '2'),
+('12', '2023-09-19', '16:00:00', 'ola', '16', '1'),
+('13', '2023-09-19', '16:15:00', 'ya yo ya', '1', '3'),
+('14', '2023-09-19', '16:30:00', 'jaijsiajsijijsi', '22', '2'),
+('15', '2023-09-19', '16:45:00', 'no', '25', '2'),
+('16', '2023-09-19', '17:00:00', 'ayer me tome clorox', '1', '1'),
+('17', '2023-09-19', '17:15:00', 'no es excusas ', '22', '1'),
+('18', '2023-09-19', '17:30:00', 'es problema del indio', '7', '1'),
+('19', '2023-09-19', '17:45:00', 'la defeco', '10', '1'),
+('20', '2023-09-19', '18:00:00', 'no se chino', '13', '1'),
+('21', '2023-09-19', '18:15:00', 'colombia es gooood', '2', '1'),
+('22', '2023-09-19', '18:30:00', 'viva cristo rey', '19', '1'),
+('23', '2023-09-19', '18:45:00', 'nico lindo', '1', '2'),
+('24', '2023-09-19', '19:00:00', 'jhoan mi rey', '25', '3'),
+('25', '2023-09-19', '19:15:00', 'jhuan mi x100pre', '1', '3'),
+('26', '2023-09-19', '19:30:00', 'ilovemi', '22', '1');
 
 
 # Traemos el nombre de usuario y el rol
@@ -277,8 +291,4 @@ LEFT JOIN usuarios_grupos c ON a.id_grupos = c.id_grupos
 #WHERE c.numerodoc = '1131104356';
 
 # Con esta consulta traeremos los datos de los mensajes
-# (segun el grupo al cual pertenezcan y en el que el usuario se ubica)
--- SELECT DISTINCT a.numerodoc, primer_nom, primer_apellido, contenido_mensaje, hora, id_tipo, id_usuarios_grupos FROM usuarios a
--- RIGHT JOIN usuarios_grupos b ON a.numerodoc = b.numerodoc
--- INNER JOIN mensaje c ON b.id_grupos = c.fk_id_grupos
--- WHERE b.id_usuarios_grupos = 1;
+# (segun el grupo al cual pertenezcan y en el que el usuario se ubica)   # SELECT DISTINCT a.numerodoc, primer_nom, primer_apellido, contenido_mensaje, hora, id_tipo, id_usuarios_grupos FROM usuarios a   # RIGHT JOIN usuarios_grupos b ON a.numerodoc = b.numerodoc   # INNER JOIN mensaje c ON b.id_grupos = c.fk_id_grupos   # WHERE b.id_usuarios_grupos = 1;
