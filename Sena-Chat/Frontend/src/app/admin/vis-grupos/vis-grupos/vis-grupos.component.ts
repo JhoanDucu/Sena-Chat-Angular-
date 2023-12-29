50431dc9c926e347ef075e6aaa2294b33a0f52c3
JUAN CARDENAS PEREZ <114235553+Juanperez23215698@users.noreply.github.com>
2023-12-29 14:48:30 -0500

    Admin
    se añadio la vista de crear y de visualizar grupos

---
+++ Sena-Chat/Frontend/src/app/admin/vis-grupos/vis-grupos/vis-grupos.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vis-grupos',
+  standalone: true,
+  imports: [],
+  templateUrl: './vis-grupos.component.html',
+  styleUrl: './vis-grupos.component.css'
+})
+export class VisGruposComponent {
+
+}
