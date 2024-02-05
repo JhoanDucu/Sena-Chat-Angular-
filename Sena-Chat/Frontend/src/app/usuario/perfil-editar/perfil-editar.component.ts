f0dfa71bc20574489ec33abe97c2f6087444995c
JhoanDucu <johanandreyd@gmail.com>
2024-02-05 17:15:50 -0500

    Añadiendo el primer editar de perfil
    Proximamente el backend y ver que otros diseños reparar.

--- Sena-Chat/Frontend/src/app/usuario/perfil-editar/perfil-editar.component.ts
+++ Sena-Chat/Frontend/src/app/usuario/perfil-editar/perfil-editar.component.ts
@@ -1,18 +1,35 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Usuario } from '../Modelos/usuarios';
+import { EditarDirective } from '../Directivas/editar.directive';
 
 @Component({
   selector: 'app-perfil-editar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EditarDirective],
   templateUrl: './perfil-editar.component.html',
   styleUrl: './perfil-editar.component.css'
 })
 export class PerfilEditarComponent {
   @Input() usuario: Usuario = {};
   hoverImg = false;
-  nombre_usuario = false;
+  propsEditar: any = {
+    correo: false,
+    primer_nom: false,
+    segundo_nom: false,
+    primer_apellido: false,
+    segundo_apellido: false,
+    contrasena: false,
+    nombre_usuario: false,
+    descripcion: false,
+    numerodoc: false,
+    fk_id_tipodoc: false,
+    id_fichas: false,
+    foto: false,
+    fk_id_rol: false
+  };
 
   activarBoton = () => this.hoverImg = !this.hoverImg;
+
+  activarEditar = (prop: any) => this.propsEditar[prop] = !this.propsEditar[prop];
 }
