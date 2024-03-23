647b3a97a01dde221cbdddbb73695370ac524b13
JhoanDucu <johanandreyd@gmail.com>
2024-03-22 19:56:19 -0500

    Trabajar el visualizar grupos (admin)

--- Sena-Chat/Frontend/src/app/administrador/principal/principal.component.ts
+++ Sena-Chat/Frontend/src/app/administrador/principal/principal.component.ts
@@ -1,43 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CrearGrupoService } from '../Servicios/crear-grupo.service';
-import { CrearGrupoComponent } from '../grupos/crear-grupo/crear-grupo.component';
 import { VisGruposComponent } from '../grupos/vis-grupos/vis-grupos.component';
-import { EditarGrupoComponent } from '../grupos/editar-grupo/editar-grupo.component';
 import { VisUsuarioComponent } from '../usuarios/vis-usuario/vis-usuario.component';
-import { CrearUsuarioComponent } from '../usuarios/crear-usuario/crear-usuario.component';
-import { EditarUsuarioComponent } from '../usuarios/editar-usuario/editar-usuario.component';
 import { VisMensajeComponent } from '../mensajes/vis-mensaje/vis-mensaje.component';
-import { CrearMensajeComponent } from '../mensajes/crear-mensaje/crear-mensaje.component';
-import { EditarMensajeComponent } from '../mensajes/editar-mensaje/editar-mensaje.component';
 import { VisFichaComponent } from '../fichas/vis-ficha/vis-ficha.component';
-import { CrearFichaComponent } from '../fichas/crear-ficha/crear-ficha.component';
-import { EditarFichaComponent } from '../fichas/editar-ficha/editar-ficha.component';
 
 @Component({
   selector: 'app-principal',
   standalone: true,
   imports: [
     CommonModule,
-    CrearGrupoComponent,
     VisGruposComponent,
-    EditarGrupoComponent,
     VisUsuarioComponent,
-    CrearUsuarioComponent,
-    EditarUsuarioComponent,
     VisMensajeComponent,
-    CrearMensajeComponent,
-    EditarMensajeComponent,
-    VisFichaComponent,
-    CrearFichaComponent,
-    EditarFichaComponent
+    VisFichaComponent
   ],
   templateUrl: './principal.component.html',
   styleUrls: ['./principal.component.css']
 })
 export class PrincipalComponent implements OnInit {
 
-  constructor(private crearGrupoService: CrearGrupoService) {
+  constructor() {
     this.message = '';
   }
 
@@ -45,9 +28,7 @@ export class PrincipalComponent implements OnInit {
 
   message!: string;
 
-  ngOnInit() {
-    // this.crearGrupoService.currentMessage.subscribe((message: string) => this.message = message);
-  }
+  ngOnInit() { }
 
   seleccionarOpcion = (opcion: string) => this.opcion = opcion;
 
