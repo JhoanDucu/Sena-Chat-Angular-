379e8134ec85226c2664ba57d5178e97596d3af2
JhoanDucu <johanandreyd@gmail.com>
2024-03-31 22:04:29 -0500

    Fusion de cambios actuales y ajustes de Juan

--- Sena-Chat/Frontend/src/app/usuario/mensajes/mensajes.component.ts
+++ Sena-Chat/Frontend/src/app/usuario/mensajes/mensajes.component.ts
@@ -4,6 +4,7 @@ import { MensajeMostrar } from '../../Modelos/mensaje';
 import { SesionService } from '../Sesiones/sesion.service';
 import { ImagenDirective } from '../Directivas/imagen.directive';
 import { url } from '../../../servidor';
+import { Fecha } from '../../Modelos/fechas';
 
 @Component({
   selector: 'app-mensajes',
@@ -25,25 +26,36 @@ export class MensajesComponent {
   fichaSeleccionada = this.Sesion.get('ficha');
   usuario = this.Sesion.get('documento');
   url = url + '/imagenes/';
+  mostrarBotonScroll = false;
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['mensajeFinal'] && !changes['mensajeFinal'].firstChange) setTimeout(() => this.hacerScroll(2), 0);
   }
 
   ngAfterViewInit(): void {
-    if (this.mensajes.length !== 0) this.hacerScroll(1);
+    if (this.mensajes.length !== 0) setTimeout(() => this.hacerScroll(1), 0);
   }
 
   ngOnDestroy(): void {
     // console.log('do something, cuando se destruye');
   }
 
-  obtenerHora = (date: Date) => `${date.toLocaleTimeString('en-US', { hour: "2-digit", minute: "2-digit" })}`;
+  obtenerHora = (date: Date) => Fecha.obtenerHora(date);
 
   hacerScroll = (behavior: 1 | 2) => {
     this.mensajesElement.nativeElement.scrollTo({
       top: this.mensajesElement.nativeElement.scrollHeight,
       behavior: behavior == 1 ? 'auto' : 'smooth',
     });
+  }
+
+  onScroll(event: any) {
+    const container = event.target;
+    const scrollTop = container.scrollTop;
+    const scrollHeight = container.scrollHeight;
+    const clientHeight = container.clientHeight;
+    const distanceFromBottom = 400; // Define la distancia desde la parte inferior
+
+    this.mostrarBotonScroll = scrollTop <= scrollHeight - clientHeight - distanceFromBottom;
   }
 }
